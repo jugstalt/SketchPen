6a184cb64bd3d35a10bc70a56c6a5044f599ac48
jugstalt <gstalt@gmx.at>
2022-03-17 15:54:23 +0100

    bat

--- plot/plot.bat
+++ plot/plot.bat
@@ -2,4 +2,4 @@ echo off
 
 C:\Development_OpenSource\github\jugstalt\SketchPen\src\SketchPen\bin\Debug\net6.0\SketchPen.exe %1 -outfolder %1-img -custom_globals %2
 
-rem G:\github\jugstalt\SketchPen\src\SketchPen\bin\Debug\net6.0\SketchPen.exe %1 -outfolder %1-img -custom_globals %2
+:: G:\github\jugstalt\SketchPen\src\SketchPen\bin\Debug\net6.0\SketchPen.exe %1 -outfolder %1-img -custom_globals %2

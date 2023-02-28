13bc9f9a6b0f196f79eedd183733f38b27e510fb
jugstalt <gstalt@gmx.at>
2023-02-28 22:04:27 +0100

    sync

--- src/SketchPen.Plot.Skia/PlotPath.cs
+++ src/SketchPen.Plot.Skia/PlotPath.cs
@@ -3,83 +3,69 @@ using SketchPen.Plot.Skia.Extensions;
 using SkiaSharp;
 using System.Collections.Generic;
 
-namespace SketchPen.Plot.Skia
+namespace SketchPen.Plot.Skia;
+
+class PlotPath : IPlotPath
 {
-    class PlotPath : IPlotPath
+    private SKPath _path;
+    private bool _startFigure = true;
+
+    public PlotPath()
     {
-        private SKPath _path;
-        private bool _startFigure = true;
-
-        public PlotPath()
+        _path = new SKPath()
         {
-            _path = new SKPath()
-            {
-                FillType = SKPathFillType.EvenOdd
-            };
+            FillType = SKPathFillType.EvenOdd
+        };
 
-            _startFigure = true;
+        _startFigure = true;
+    }
+
+    #region IPlotPath
+
+    public object EngineElement => _path;
+
+    public void Start()
+    {
+        _startFigure = true;
+    }
+
+    public void Close()
+    {
+        _path.Close();
+        _startFigure = true;
+    }
+
+    public void AddArc(CanvasRectangle rect, float startAngle, float sweepAngle)
+    {
+        if (_startFigure)
+        {
+            _startFigure = false;
+            _path.AddArc(rect.ToSKRect(), startAngle, sweepAngle);
+        }
+        else
+        {
+            _path.ArcTo(rect.ToSKRect(), startAngle, sweepAngle, false);
         }
 
-        #region IPlotPath
-
-        public object EngineElement => _path;
-
-        public void Start()
+        if (sweepAngle >= 360.0)
         {
-            _startFigure = true;
+            this.Close(); // to have the same behavoir as with System.Drawing
+        }
+    }
+
+    public void AddLines(IEnumerable<CanvasPoint> points)
+    {
+        if (points == null)
+        {
+            return;
         }
 
-        public void Close()
+        bool first = _startFigure;
+        foreach (var point in points)
         {
-            _path.Close();
-            _startFigure = true;
-        }
-
-        public void AddArc(CanvasRectangle rect, float startAngle, float sweepAngle)
-        {
-            if (_startFigure)
+            if (first)
             {
-                _startFigure = false;
-                _path.AddArc(rect.ToSKRect(), startAngle, sweepAngle);
-            }
-            else
-            {
-                _path.ArcTo(rect.ToSKRect(), startAngle, sweepAngle, false);
-            }
-
-            if (sweepAngle >= 360.0)
-            {
-                this.Close(); // to have the same behavoir as with System.Drawing
-            }
-        }
-
-        public void AddLines(IEnumerable<CanvasPoint> points)
-        {
-            if (points == null)
-            {
-                return;
-            }
-
-            bool first = _startFigure;
-            foreach (var point in points)
-            {
-                if (first)
-                {
-                    _startFigure = first = false;
-                    _path.MoveTo(point.ToSKPoint());
-                }
-                else
-                {
-                    _path.LineTo(point.ToSKPoint());
-                }
-            }
-        }
-
-        public void AddPoint(CanvasPoint point)
-        {
-            if (_path.PointCount == 0)
-            {
-                _startFigure = false;
+                _startFigure = first = false;
                 _path.MoveTo(point.ToSKPoint());
             }
             else
@@ -87,20 +73,33 @@ namespace SketchPen.Plot.Skia
                 _path.LineTo(point.ToSKPoint());
             }
         }
-
-        #endregion
-
-        #region IDisposable
-
-        public void Dispose()
-        {
-            if (_path != null)
-            {
-                _path.Dispose();
-                _path = null;
-            }
-        }
-
-        #endregion
     }
+
+    public void AddPoint(CanvasPoint point)
+    {
+        if (_path.PointCount == 0)
+        {
+            _startFigure = false;
+            _path.MoveTo(point.ToSKPoint());
+        }
+        else
+        {
+            _path.LineTo(point.ToSKPoint());
+        }
+    }
+
+    #endregion
+
+    #region IDisposable
+
+    public void Dispose()
+    {
+        if (_path != null)
+        {
+            _path.Dispose();
+            _path = null;
+        }
+    }
+
+    #endregion
 }
